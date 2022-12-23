8ee6075427617bbc12fb991c13d7445ea3640a7e
Noah Childress <nchildress@css.edu>
2022-12-22 18:43:30 -0600

    Final Version. No Errors. Filtering Works.

--- src/ApplicationCore/Entities/CatalogItem.cs
+++ src/ApplicationCore/Entities/CatalogItem.cs
@@ -10,16 +10,16 @@ namespace Microsoft.eShopWeb.ApplicationCore.Entities
         public string Description { get; private set; }
         public decimal Price { get; private set; }
         public string PictureUri { get; private set; }
-        public int CatalogTypeId { get; private set; }
-        public CatalogType CatalogType { get; private set; }
         public int CatalogBrandId { get; private set; }
         public CatalogBrand CatalogBrand { get; private set; }
+        public int CatalogTypeId { get; private set; }
+        public CatalogType CatalogType { get; private set; }
         public int CatalogPriceId { get; private set; }
         public CatalogBrand CatalogPrice { get; private set; }
         public string ItemColor { get; private set; }
 
-        public CatalogItem(int catalogTypeId,
-            int catalogBrandId,
+        public CatalogItem(int catalogBrandId,
+            int catalogTypeId,
             int catalogPriceId,
             string description,
             string name,
@@ -27,8 +27,8 @@ namespace Microsoft.eShopWeb.ApplicationCore.Entities
             decimal price,
             string pictureUri)
         {
-            CatalogTypeId = catalogTypeId;
             CatalogBrandId = catalogBrandId;
+            CatalogTypeId = catalogTypeId;
             CatalogPriceId = catalogPriceId;
             Description = description;
             Name = name;
--- src/Infrastructure/Data/CatalogContextSeed.cs
+++ src/Infrastructure/Data/CatalogContextSeed.cs
@@ -68,8 +68,8 @@ namespace Microsoft.eShopWeb.Infrastructure.Data
         {
             return new List<CatalogBrand>
             {
-                new("Clothing"),
                 new("Accessories"),
+                new("Clothing"),
                 new("Drinkware")
             };
         }
@@ -78,14 +78,14 @@ namespace Microsoft.eShopWeb.Infrastructure.Data
         {
             return new List<CatalogType>
             {
-                new("Sweatshirts"),
-                new("Crew Necks"),
-                new("Tees & Tops"),
-                new("Bottoms"),
-                new("Face Masks"),
                 new("Blankets"),
                 new("Bottles"),
-                new("Cups")
+                new("Bottoms"),
+                new("Crew Necks"),
+                new("Cups"),
+                new("Face Masks"),
+                new("Sweatshirts"),
+                new("Tees & Tops")
             };
         }
 
@@ -93,9 +93,8 @@ namespace Microsoft.eShopWeb.Infrastructure.Data
         {
             return new List<CatalogPrice>
             {
-                new("$0.99 - 4.99"),
-                new("$5.00 - 14.99"),
-                new("$15.00 - 24.99"),
+                new("$0.99 - 9.99"),
+                new("$10.00 - 24.99"),
                 new("$25.00 - 34.99"),
                 new("$35.00 - 49.99"),
                 new("$50.00+")
@@ -106,21 +105,21 @@ namespace Microsoft.eShopWeb.Infrastructure.Data
         {
             return new List<CatalogItem>
             {
-                new(1,1,6, "League Essential Fleece Hood", "League Essential Fleece Hood", "Navy", 54.99M,  "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/d98e9824eafcdea23fa8d71f9dc22cc1_400x.jpg?v=1670533441"),
-                new(1,1,5, "CI Sport Storm Hood", "CI Sport Storm Hood", "Sweet Ash", 49.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/image_261e7d5b-aad3-4274-8e77-46064295dc38_400x.png?v=1667058356"),//Sweet Ash
-                new(1,2,6,"Gear Outta Town Crew", "Gear Outta Town Crew", "Light Blue", 54.99M,  "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/aac9023a831d39e5c48d37a641e08f0a_400x.jpg?v=1666904635"),//Light Blue
-                new(1,2,5,"CI Sport Crewneck Storm", "CI Sport Crewneck Storm", "Sweet Mustard", 47.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/b587b01ebaef054337f64e67c7d4c5c9_400x.jpg?v=1668762285"),//Sweet Mustard
-                new(1,3,3,"MV Sport Everest Sustainable Tee", "MV Sport Everest Sustainable Tee", "Dark Grey", 18.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/b3debab5239ee857fb7bed84682ce54a_400x.jpg?v=1669755808"),//Dark Grey
-                new(1,3,5,"Nike Dri-Fit Cotton Long Sleeve Tee", "Nike Dri-Fit Cotton Long Sleeve Tee", "White", 42.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/0028fdcbaf70488838949f2c910f62bc_400x.jpg?v=1662138249"),//White
-                new(1,4,4,"MV Sport Vintage Fleece Pant With Leg Graphic", "MV Sport Vintage Fleece Pant With Leg Graphic", "Black", 34.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/8e3146e65faa1b76a7f6f25bbc1a7a80_400x.jpg?v=1669759417"),//Black
-                new(1,4,6,"Nike Club Fleece Jogger", "Nike Club Fleece Jogger", "Black", 65.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/af4e21f40652371b4bfa94ecb82352a0_400x.jpg?v=1666083965"),//Black
-                new(2,5,1,"League Facemask", "League Facemask", "Blue/Yellow", 3.25M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/e4071fd90f4eeec37f47878645e03170_400x.jpg?v=1605562635"),//Blue/Yellow
-                new(2,5,1,"League Heather Champ Face Covering", "League Heather Champ Face Covering", "Navy/Gold", 3.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/a8edef95b23b76d2808d4b7bfd5abe69_400x.jpg?v=1634829401"),//Navy/Gold
-                new(2,6,5,"MV Sport Pro-Weave Sweatshirt Blanket", "MV Sport Pro-Weave Sweatshirt Blanket", "Navy", 44.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/4778081-3300-001-COLL0821P_400x.jpg?v=1661372900"),//Navy
-                new(3,7,3,"Spirit Game Day Shaker Bottle", "Spirit Game Day Shaker Bottle", "Navy", 17.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/fa50f4b5f0cd6cc8f3fb045464a0227c_400x.jpg?v=1657731811"),//Navy
-                new(3,7,3,"Spirit Omni Copper Lined Thermal Tumbler", "Spirit Omni Copper Lined Thermal Tumbler", "Navy", 19.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/3eca0d84076096d7f8d384811b63af9a_400x.jpg?v=1614938624"),//Navy
-                new(3,8,2,"Traditional Coffee Mug", "Traditional Coffee Mug", "Navy", 12.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/e04dcfc93a945ea15c72fc2ee61b6d5c_400x.jpg?v=1637270049"),//Navy
-                new(3,8,2,"Minolo Mug With Yellow Interior", "Minolo Mug", "White", 14.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/f800ff89943a2d41561e5dea0999259b_400x.jpg?v=1637270061")//White
+                new(2,7,5,"League Essential Fleece Hood", "League Essential Fleece Hood", "Navy", 54.99M,  "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/d98e9824eafcdea23fa8d71f9dc22cc1_400x.jpg?v=1670533441"),
+                new(2,7,4,"CI Sport Storm Hood", "CI Sport Storm Hood", "Sweet Ash", 49.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/image_261e7d5b-aad3-4274-8e77-46064295dc38_400x.png?v=1667058356"),
+                new(2,4,5,"Gear Outta Town Crew", "Gear Outta Town Crew", "Light Blue", 54.99M,  "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/aac9023a831d39e5c48d37a641e08f0a_400x.jpg?v=1666904635"),
+                new(2,4,4,"CI Sport Crewneck Storm", "CI Sport Crewneck Storm", "Sweet Mustard", 47.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/b587b01ebaef054337f64e67c7d4c5c9_400x.jpg?v=1668762285"),
+                new(2,8,2,"MV Sport Everest Sustainable Tee", "MV Sport Everest Sustainable Tee", "Dark Grey", 18.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/b3debab5239ee857fb7bed84682ce54a_400x.jpg?v=1669755808"),
+                new(2,8,4,"Nike Dri-Fit Cotton Long Sleeve Tee", "Nike Dri-Fit Cotton Long Sleeve Tee", "White", 42.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/0028fdcbaf70488838949f2c910f62bc_400x.jpg?v=1662138249"),
+                new(2,3,3,"MV Sport Vintage Fleece Pant With Leg Graphic", "MV Sport Vintage Fleece Pant With Leg Graphic", "Black", 34.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/8e3146e65faa1b76a7f6f25bbc1a7a80_400x.jpg?v=1669759417"),
+                new(2,3,5,"Nike Club Fleece Jogger", "Nike Club Fleece Jogger", "Black", 65.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/af4e21f40652371b4bfa94ecb82352a0_400x.jpg?v=1666083965"),
+                new(1,6,1,"League Facemask", "League Facemask", "Blue/Yellow", 3.25M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/e4071fd90f4eeec37f47878645e03170_400x.jpg?v=1605562635"),
+                new(1,6,1,"League Heather Champ Face Covering", "League Heather Champ Face Covering", "Navy/Gold", 3.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/a8edef95b23b76d2808d4b7bfd5abe69_400x.jpg?v=1634829401"),
+                new(1,1,4,"MV Sport Pro-Weave Sweatshirt Blanket", "MV Sport Pro-Weave Sweatshirt Blanket", "Navy", 44.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/4778081-3300-001-COLL0821P_400x.jpg?v=1661372900"),
+                new(3,2,2,"Spirit Game Day Shaker Bottle", "Spirit Game Day Shaker Bottle", "Navy", 17.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/fa50f4b5f0cd6cc8f3fb045464a0227c_400x.jpg?v=1657731811"),
+                new(3,2,2,"Spirit Omni Copper Lined Thermal Tumbler", "Spirit Omni Copper Lined Thermal Tumbler", "Navy", 19.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/3eca0d84076096d7f8d384811b63af9a_400x.jpg?v=1614938624"),
+                new(3,5,2,"Traditional Coffee Mug", "Traditional Coffee Mug", "Navy", 12.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/e04dcfc93a945ea15c72fc2ee61b6d5c_400x.jpg?v=1637270049"),
+                new(3,5,2,"Minolo Mug With Yellow Interior", "Minolo Mug", "White", 14.99M, "https://cdn.shopify.com/s/files/1/0008/6491/1404/products/f800ff89943a2d41561e5dea0999259b_400x.jpg?v=1637270061")
             };
         }
     }
